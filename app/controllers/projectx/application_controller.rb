6978fb5710a40ad5ea6e871a1f2a0e659ecd01b3
emclab <emclab2011@gmail.com>
2013-04-03 21:52:11 -0500

    AC: deleted a merge problem

--- app/controllers/projectx/application_controller.rb
+++ app/controllers/projectx/application_controller.rb
@@ -10,20 +10,6 @@ module Projectx
 
     before_filter :check_access_right
 
-<<<<<<< HEAD
-=======
-
-
-    def check_access_right
-      return false if session[:user_privilege].nil? 
-      actionsMap = {'new' => 'create', 'edit' => 'update'}
-      #here we need to use our new access control framework. For now le's re-use what we currently have
-      if !checkAndUpdateParam(params['controller'].sub('/', '_'), params['action'], actionsMap)
-        redirect_to URI.escape(SUBURI + "/authentify/view_handler?index=0&msg=Insufficient Access Right!")
-      end
-    end
-
->>>>>>> cf39089d252265f03e316d6f2953da5d549ca3e1
     protected
 
     def page(models)
