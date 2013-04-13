aa8fdf8ce9489ec777b266758c2c70b9a135363d
jc <emclab2011@gmail.com>
2013-04-13 07:39:11 -0500

    JC: added default to task template

--- db/migrate/20130413034053_create_projectx_task_templates.rb
+++ db/migrate/20130413034053_create_projectx_task_templates.rb
@@ -5,9 +5,9 @@ class CreateProjectxTaskTemplates < ActiveRecord::Migration
       t.integer :task_definition_id
       t.integer :execution_order
       t.integer :execution_sub_order
-      t.boolean :start_before_previous_completed
+      t.boolean :start_before_previous_completed, :default => false
       t.text :brief_note
-      t.boolean :need_request
+      t.boolean :need_request, :default => false
 
       t.timestamps
     end
