627fd5e8e4bfba610e2c934df495b69f5149a4f7
emclab <emclab2011@gmail.com>
2013-04-16 14:20:18 -0500

    Update 20130413033455_create_projectx_project_templates.rb

--- db/migrate/20130413033455_create_projectx_project_templates.rb
+++ db/migrate/20130413033455_create_projectx_project_templates.rb
@@ -2,7 +2,7 @@ class CreateProjectxProjectTemplates < ActiveRecord::Migration
   def change
     create_table :projectx_project_templates do |t|
       t.string :name
-      t.integer :project_type_id
+      t.integer :type_definition_id
       t.integer :last_updated_by_id
       t.boolean :active, :default => true
       t.text :instruction
