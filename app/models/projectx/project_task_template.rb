29e5e75d780b1d757eaffa624a0a5ead0dfd0777
jc <emclab2011@gmail.com>
2013-06-10 12:22:31 -0500

    JC: fixed bug in project task templat show/edit.

--- app/models/projectx/project_task_template.rb
+++ app/models/projectx/project_task_template.rb
@@ -1,12 +1,12 @@
 module Projectx
   class ProjectTaskTemplate < ActiveRecord::Base
-    attr_accessible :active, :instruction, :last_updated_by_id, :name, :type_definition_id, :ranking_order, :as => :role_new
-    attr_accessible :active, :instruction, :last_updated_by_id, :name, :type_definition_id, :ranking_order, :as => :role_update
+    attr_accessible :active, :instruction, :last_updated_by_id, :name, :type_definition_id, :ranking_order, :task_templates_attributes, :as => :role_new
+    attr_accessible :active, :instruction, :last_updated_by_id, :name, :type_definition_id, :ranking_order, :task_templates_attributes, :as => :role_update
     
     belongs_to :type_definition, :class_name => 'Projectx::TypeDefinition'
     belongs_to :last_updated_by, :class_name => 'Authentify::User' 
     has_many :task_templates, :class_name => 'Projectx::TaskTemplate'
-    accepts_nested_attributes_for :task_templates, :reject_if => proc {|task| task['task_definition_id'].blank? }, :allow_destroy => true
+    accepts_nested_attributes_for :task_templates, :allow_destroy => true  #data validate in task_template model
        
     validates :type_definition_id, :presence => true,
                                 :numericality => {:greater_than => 0}
--- app/models/projectx/task_template.rb
+++ app/models/projectx/task_template.rb
@@ -4,13 +4,16 @@ module Projectx
     attr_accessible :brief_note, :execution_order, :execution_sub_order, :need_request, :project_task_template_id, :start_before_previous_completed, 
                     :task_definition_id,
                     :as => :role_new
+   attr_accessible :brief_note, :execution_order, :execution_sub_order, :need_request, :project_task_template_id, :start_before_previous_completed, 
+                    :task_definition_id,
+                    :as => :role_update                 
     
     belongs_to :project_task_template, :class_name => 'Projectx::ProjectTaskTemplate'
     belongs_to :last_updated_by, :class_name => 'Authentify::User' 
     belongs_to :task_definition, :class_name => 'Projectx::TaskDefinition'
     
-    validates :project_task_template_id, :presence => true,
-                                         :numericality => {:greater_than => 0, :message => '必须 > 0'}
+    #validates :project_task_template_id, :presence => true,
+      #                                   :numericality => {:greater_than => 0, :message => '必须 > 0'}
     validates :execution_order, :presence => true,
                                 :numericality => {:greater_than => 0, :message => '必须 > 0'}
     validates :task_definition_id, :presence => true,
